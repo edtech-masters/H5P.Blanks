6dbd68adfd137a1ac0a6eb11b15f7e716d7f4ec2
yuvaraj <yuvaraj@curriki.org>
2022-01-03 17:44:27 +0530

    Added xapi 'completed' statement

--- js/blanks.js
+++ js/blanks.js
@@ -206,13 +206,16 @@ H5P.Blanks = (function ($, Question) {
         self.toggleButtonVisibility(STATE_CHECKING);
 
         if(typeof self.parent == "undefined") {
+          // trigger completed
+          self.triggerCompleted();
+          
           self.triggerXAPIScored(self.getScore(), self.getMaxScore(), 'submitted-curriki');
         }
         self.markResults();
         self.showEvaluation();
         
         self.triggerAnswered();
-        
+
       }, true, {
         'aria-label': self.params.a11yCheck,
       }, {
@@ -404,6 +407,8 @@ H5P.Blanks = (function ($, Question) {
             self.toggleButtonVisibility(STATE_CHECKING);
             self.showEvaluation();
             self.triggerAnswered();
+            // trigger completed
+            self.triggerCompleted();
             self.done = true;
           }
         };
@@ -721,10 +726,7 @@ H5P.Blanks = (function ($, Question) {
    * Trigger xAPI completed event
    */
    Blanks.prototype.triggerCompleted = function () {
-    var xAPIEvent = this.createXAPIEventTemplate('answered');
-    this.addQuestionToXAPI(xAPIEvent);
-    this.addResponseToXAPI(xAPIEvent);
-    this.trigger(xAPIEvent);
+    self.triggerXAPICompleted(self.getScore(), self.getMaxScore());
   };
 
 
